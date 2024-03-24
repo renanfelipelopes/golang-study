d1a9015fc953400d5bdc186eebc4044c55a17e98
Renan Lopes <renanlopes.ads@gmail.com>
2024-03-24 18:19:37 -0300

    Maps e Funcoes

--- 7/main.go
+++ 7/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ID int
 
 var (
@@ -12,5 +14,21 @@ var (
 
 // trabalhando com maps aula F015
 func main() {
+	salarios := map[string]int{"Renan": 1000, "Joao": 500, "Maria": 250}
+	delete(salarios, "Renan")
 
+	// salar := make(map[string]int)
+	// salar1 := map[string]int{}
+	// salar1["RenanF"] = 1500
+	// fmt.Println(salar)
+	// fmt.Println(salar1)
+
+	for nome, salario := range salarios {
+		fmt.Printf("O salario de %s é %d\n", nome, salario)
+	}
+
+	// se eu quiser exibir apenas o salario e o nome não, eu posso usa o 'blank identifier' que é o underline _:
+	for _, salario := range salarios {
+		fmt.Printf("O salario é %d\n", salario)
+	}
 }
