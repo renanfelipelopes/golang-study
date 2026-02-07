e9211ec6124bb8bcdea90d9882d87f41ce045b9a
Renan Lopes <renanlopes.ads@gmail.com>
2026-02-06 21:28:24 -0300

    feat: Metodos em Structs

---
+++ Pós Go Expert/Módulo - Fundação/13-Métodos_em_Structs.go
@@ -0,0 +1,35 @@
+/*
+	Aula: Métodos em Structs
+*/
+
+package main
+
+import "fmt"
+
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	Endereco
+}
+
+func (c Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("O cliente %s foi desativado.", c.Nome)
+}
+
+func main() {
+	renan := Cliente{
+		Nome:  "Renan",
+		Idade: 28,
+		Ativo: true,
+	}
+	renan.Desativar()
+}
