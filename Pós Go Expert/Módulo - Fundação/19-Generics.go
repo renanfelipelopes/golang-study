d95bc8f327833b6ab011dd8cf8f5ac4814cf32f9
Renan Lopes <renanlopes.ads@gmail.com>
2026-02-16 10:50:23 -0300

    feat: Generics

---
+++ Pós Go Expert/Módulo - Fundação/19-Generics.go
@@ -0,0 +1,15 @@
+/*
+	Aula: Generics (Go 1.18+)
+
+	Generics permitem escrever código reutilizável com segurança de tipo
+	em tempo de compilação.
+
+	Antes dos Generics:
+	- Precisávamos duplicar funções para cada tipo.
+	- Ou usar interface{} e perder segurança de tipo.
+
+	Com Generics:
+	- Código reutilizável
+	- Type safety
+	- Melhor performance que interface{}
+*/
