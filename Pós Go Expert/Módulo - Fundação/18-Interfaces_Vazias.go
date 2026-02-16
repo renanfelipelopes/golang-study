06e6ffa0f663ae45ffc51bd6accddc20fdd9ac5c
Renan Lopes <renanlopes.ads@gmail.com>
2026-02-15 22:35:22 -0300

    feat: type assertation

--- Pós Go Expert/Módulo - Fundação/18-Interfaces_Vazias.go
+++ Pós Go Expert/Módulo - Fundação/18-Interfaces_Vazias.go
@@ -1,7 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	var minhaVar interface{} = "Renan Lopes"
-
 	println(minhaVar.(string))
+
+	res, ok := minhaVar.(int)
+	fmt.Printf("O valor de res é %v e o resultado de ok é %v", res, ok)
 }
