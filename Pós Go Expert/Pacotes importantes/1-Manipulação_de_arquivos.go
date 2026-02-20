8d69b9000161ec2e7f6f6a7164d9b904b3f00ea8
Renan Lopes <renanlopes.ads@gmail.com>
2026-02-19 21:27:49 -0300

    feat: criando manipulador de arquivo - leitura

--- Pós Go Expert/Pacotes importantes/1-Manipulação_de_arquivos.go
+++ Pós Go Expert/Pacotes importantes/1-Manipulação_de_arquivos.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -11,11 +12,39 @@ func main() {
 		panic(err)
 	}
 
+	/// escrita
 	tamanho, err := f.Write([]byte("Escrevendo dados no arquivo..."))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Arquivo criado com sucesso! Tamanho: %d bytes", tamanho)
+	fmt.Printf("Arquivo criado com sucesso! Tamanho: %d bytes\n", tamanho)
 	f.Close()
 
+	/// leitura
+	arquivo, err := os.ReadFile("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(arquivo))
+
+	/// leitura em lotes de arquivo grande
+	arquivo2, err := os.Open("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(arquivo2)
+	buffer := make([]byte, 3)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(buffer[:n]))
+	}
+
+	err = os.Remove("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
 }
