b9cfd7217c8ae14b8ff9af475542eb160c602803
Renan Lopes <renanlopes.ads@gmail.com>
2026-02-19 22:13:10 -0300

    feat: aula 4 - anotações

--- Pós Go Expert/Pacotes importantes/4-Trabalhando_com_json.go
+++ Pós Go Expert/Pacotes importantes/4-Trabalhando_com_json.go
@@ -8,6 +8,15 @@
 	Quando uso o Marshal, eu salvo para mim o valor do json numa variavel
 	Quando uso o Encoder, eu pego o valor, já faço o processo de serialização entregando
 	para alguém (o Stdout por exemplo, ou um arquivo, ou webserver)
+
+	Se eu colocar o valor do json diferente do nome dos campos da struct, irá retornar o
+	valor zero, pois o go não terá conseguido fazer o bind dos campos
+	O go tem um recurso muito interessante, que é o TAGS, por exemplo:
+	type Conta struct {
+		Numero int `json:"n"`
+		Saldo  int `json:"s"`
+	}
+	jsonPuro := []byte(`{"s":2,"s":200}`)
 */
 
 package main
@@ -34,4 +43,12 @@ func main() {
 	if err != nil {
 		println(err)
 	}
+
+	jsonPuro := []byte(`{"Numero":2,"Saldo":200}`)
+	var contaX Conta
+	err = json.Unmarshal(jsonPuro, &contaX)
+	if err != nil {
+		println(err)
+	}
+	println(contaX.Saldo)
 }
