c8b26c2a167fa3638130dd6cebd633071f4758cb
Renan Lopes <renanlopes.ads@gmail.com>
2026-02-19 22:00:03 -0300

    feat: aula 4 - trabalhando com json

---
+++ Pós Go Expert/Pacotes importantes/4-Trabalhando_com_json.go
@@ -0,0 +1,30 @@
+/*
+	Aula: Trabalhando com Jsons
+	Marshal é converter em json
+
+	Podemos começar por converter uma struct em um json
+	Json sempre vai retornar em bytes
+
+	Quando uso o Marshal, eu salvo para mim o valor do json numa variavel
+	Quando uso o Encoder, eu pego o valor, já faço o processo de serialização entregando
+	para alguém (o Stdout por exemplo, ou um arquivo, ou webserver)
+*/
+
+package main
+
+import "encoding/json"
+
+type Conta struct {
+	Numero int
+	Saldo  int
+}
+
+func main() {
+	conta := Conta{Numero: 1, Saldo: 100}
+	res, err := json.Marshal(conta)
+	if err != nil {
+		println(err)
+	}
+	println(string(res))
+
+}
