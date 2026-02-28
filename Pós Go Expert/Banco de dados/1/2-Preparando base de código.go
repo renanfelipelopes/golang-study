8ab76b8839f356132b75a37234627350f17d64a7
Renan Lopes <renanlopes.ads@gmail.com>
2026-02-28 16:38:14 -0300

    feat: configurando db mysql

--- Pós Go Expert/Banco de dados/1/2-Preparando base de código.go
+++ Pós Go Expert/Banco de dados/1/2-Preparando base de código.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -17,7 +22,11 @@ func NewProduct(id, name string, price float64) *Product {
 }
 
 func main() {
-
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
 
 /*
