10cc229d998eb72445d18fc21ad29eaa05e85c3e
Renan Lopes <renanlopes.ads@gmail.com>
2026-03-16 20:46:22 -0300

    feat: importando math.go

--- Pós Go Expert/Packaging/1/cmd/main.go
+++ Pós Go Expert/Packaging/1/cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/devfullcycle/goexpert/Packaging/1/math"
+)
+
 func main() {
-	// m := math.Math{A: 1, B: 2}
-	// fmt.Println(m.Add())
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
 }
