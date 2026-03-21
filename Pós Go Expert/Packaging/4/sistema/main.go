28b52c166844c035fa145d4a5ea641579b78cdbb
Renan Lopes <renanlopes.ads@gmail.com>
2026-03-21 17:11:16 -0300

    feat: Usando workspaces

--- Pós Go Expert/Packaging/4/sistema/main.go
+++ Pós Go Expert/Packaging/4/sistema/main.go
@@ -8,6 +8,17 @@ func main() {
 }
 
 /*
-	go mod edit -replace github.com/devfullcycle/goexpert/Packaging/3/math=../math
-	go mod tidy
+	Na pasta raiz do projeto (na pasta 4 no caso) rodar o comando:
+	go work init ./math ./sistema
+
+	Porém, se adicionarmos bibliotecas externas, o go mod tidy não irá baixar,
+	pois o go work trava para apenas bibliotecas no nosso workspace.
+
+	Nessa caso, podemos publicar nossa biblioteca no github e após isso
+	rodar o go mod tidy, ou rodar o comando:
+		- go mod tidy -e
+
+	Esse comando ignora as bibliotecas que ele não consegue baixar (as nossas
+	do workspace no caso) e baixar as externas (como por exemplo a do github.com/google/uuid)
+
 */
