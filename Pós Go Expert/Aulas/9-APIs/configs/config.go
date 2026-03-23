3e600eb3bb28d9ef4f91c40211791e4f4441a157
Renan Lopes <renanlopes.ads@gmail.com>
2026-03-23 20:07:42 -0300

    feat: Finalizando configuração

--- Pós Go Expert/Aulas/9-APIs/configs/config.go
+++ Pós Go Expert/Aulas/9-APIs/configs/config.go
@@ -1,5 +1,9 @@
 package configs
 
+import (
+	"github.com/spf13/viper"
+)
+
 var cfg *conf
 
 type conf struct {
@@ -15,5 +19,17 @@ type conf struct {
 }
 
 func LoadConfig(path string) (*conf, error) {
-
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
 }
