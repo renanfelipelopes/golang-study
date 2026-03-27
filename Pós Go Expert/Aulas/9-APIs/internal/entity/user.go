9ef5656a009a43c584eae0f9b6f5bdc2f48db70b
Renan Lopes <renanlopes.ads@gmail.com>
2026-03-27 14:09:29 -0300

    Criando entidade User

---
+++ Pós Go Expert/Aulas/9-APIs/internal/entity/user.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"github.com/devfullcycle/goexpert/9-APIs/internal/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       entity.NewID(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
+}
+
+func (u *User) ValidatePassword(password srting) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
