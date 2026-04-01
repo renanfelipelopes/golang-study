85d31cbd67e7aacf9a17458011bae5301389fd59
Renan Lopes <renanlopes.ads@gmail.com>
2026-04-01 18:51:38 -0300

    Criando handler para produto

--- Pós Go Expert/Aulas/9-APIs/cmd/server/main.go
+++ Pós Go Expert/Aulas/9-APIs/cmd/server/main.go
@@ -37,12 +37,22 @@ func NewProductHandler(db database.ProductInterface) *ProductHandler {
 	}
 }
 
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, *http.Request) {
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	var product dto.CreateProductInput
 	err := json.NewDecoder(r.Body).Decode(&product)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
