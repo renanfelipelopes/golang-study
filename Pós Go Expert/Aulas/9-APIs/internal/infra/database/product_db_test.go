82da85786bbf069375e201e259dda93a7fc47c18
Renan Lopes <renanlopes.ads@gmail.com>
2026-03-31 23:44:22 -0300

    Criando testes unitarios

--- Pós Go Expert/Aulas/9-APIs/internal/infra/database/product_db_test.go
+++ Pós Go Expert/Aulas/9-APIs/internal/infra/database/product_db_test.go
@@ -93,3 +93,21 @@ func TestUpdateProduct(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "Product 2", product.Name)
 }
+
+func TestDeleteProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+
+	db.Create(product)
+	productDB := NewProduct(db)
+	err = productDB.Delete(product.ID.String())
+	assert.NoError(t, err)
+
+	_, err = productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+}
