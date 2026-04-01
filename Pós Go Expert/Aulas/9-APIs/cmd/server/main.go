9d744351df1206e6f714632e08eb0d47f4dc5389
Renan Lopes <renanlopes.ads@gmail.com>
2026-04-01 18:45:13 -0300

    Criando handler para produto

--- Pós Go Expert/Aulas/9-APIs/cmd/server/main.go
+++ Pós Go Expert/Aulas/9-APIs/cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/gorm"
 
 	"github.com/devfullcycle/goexpert/9-APIs/internal/entity"
+	"github.com/devfullcycle/goexpert/9-APIs/internal/infra/database"
 )
 
 func main() {
@@ -22,4 +23,18 @@ func main() {
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 
 	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, *http.Request) {
+	
 }
